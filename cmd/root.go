b74188b5f5f929414b4b345d0d4dd82a59535d0d
Matt Radford <mradford@protonmail.com>
2023-08-09 10:00:14 -0700

    Adjust to process each line on read

--- cmd/root.go
+++ cmd/root.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/muesli/termenv"
@@ -18,52 +17,68 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		output := termenv.NewOutput(os.Stdout)
 
-		reader := bufio.NewReader(os.Stdin)
+		scanner := bufio.NewScanner(os.Stdin)
 
-		str, err := reader.ReadString('\n')
+		for scanner.Scan() {
+			var inputLines []string
 
-		if err != nil {
-			log.Fatal(err)
+			line := scanner.Text()
+
+			inputLines = append(inputLines, line)
+
+			allInput := ""
+			if len(inputLines) > 0 {
+				allInput = inputLines[0]
+				for i := 1; i < len(inputLines); i++ {
+					allInput += "\n" + inputLines[i]
+				}
+			}
+
+			s := output.String(allInput)
+
+			if len(args) < 1 {
+				fmt.Println(s)
+				os.Exit(1)
+			}
+
+			arg_color := args[0]
+
+			colored := s.Foreground(output.Color(arg_color))
+
+			bold := viper.GetBool("bold")
+			italic := viper.GetBool("italic")
+			faint := viper.GetBool("faint")
+			strike := viper.GetBool("strike")
+			underline := viper.GetBool("underline")
+
+			if bold {
+				colored = colored.Bold()
+			}
+
+			if italic {
+				colored = colored.Italic()
+			}
+
+			if faint {
+				colored = colored.Faint()
+			}
+
+			if strike {
+				colored = colored.CrossOut()
+			}
+
+			if underline {
+				colored = colored.Underline()
+			}
+
+			fmt.Println(colored)
+
+			os.Stdout.Sync()
 		}
 
-		s := output.String(str)
-
-		if len(args) < 1 {
-			fmt.Println(s)
-			os.Exit(1)
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 		}
-
-		arg_color := args[0]
-
-		colored := s.Foreground(output.Color(arg_color))
-
-		bold := viper.GetBool("bold")
-		italic := viper.GetBool("italic")
-		faint := viper.GetBool("faint")
-		strike := viper.GetBool("strike")
-		underline := viper.GetBool("underline")
-
-		if bold {
-			colored = colored.Bold()
-		}
-
-		if italic {
-			colored = colored.Italic()
-		}
-
-		if faint {
-			colored = colored.Faint()
-		}
-
-		if strike {
-			colored = colored.CrossOut()
-		}
-
-		if underline {
-			colored = colored.Underline()
-		}
-
-		fmt.Print(colored)
 	},
 }
 
